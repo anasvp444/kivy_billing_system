850e53405a8af0bf66fa2db5a48053582d902d01
anas <anasvp444@gmail.com>
2019-12-20 12:04:15 +0800

    add user

--- backup/silverpos-master/pos_p9/operator.py
+++ backup/silverpos-master/pos_p9/operator.py
@@ -4,8 +4,7 @@ from kivy.uix.label import Label
 
 import re
 
-
-class MainoperatorWindow(BoxLayout):
+class OperatorWindow(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -17,19 +16,15 @@ class MainoperatorWindow(BoxLayout):
         pcode = self.ids.code_inp.text
         products_container = self.ids.products
         if pcode == '1234' or pcode == '2345':
-            details = BoxLayout(size_hint_y=None, height=30,
-                                pos_hint={'top': 1})
+            details = BoxLayout(size_hint_y=None,height=30,pos_hint={'top': 1})
             products_container.add_widget(details)
 
-            code = Label(text=pcode, size_hint_x=.2, color=(.06, .45, .45, 1))
-            name = Label(text='Product One', size_hint_x=.3,
-                         color=(.06, .45, .45, 1))
-            qty = Label(text='1', size_hint_x=.1, color=(.06, .45, .45, 1))
-            disc = Label(text='0.00', size_hint_x=.1, color=(.06, .45, .45, 1))
-            price = Label(text='0.00', size_hint_x=.1,
-                          color=(.06, .45, .45, 1))
-            total = Label(text='0.00', size_hint_x=.2,
-                          color=(.06, .45, .45, 1))
+            code = Label(text=pcode,size_hint_x=.2,color=(.06,.45,.45,1))
+            name = Label(text='Product One',size_hint_x=.3,color=(.06,.45,.45,1))
+            qty = Label(text='1',size_hint_x=.1,color=(.06,.45,.45,1))
+            disc = Label(text='0.00',size_hint_x=.1,color=(.06,.45,.45,1))
+            price = Label(text='0.00',size_hint_x=.1,color=(.06,.45,.45,1))
+            total = Label(text='0.00',size_hint_x=.2,color=(.06,.45,.45,1))
             details.add_widget(code)
             details.add_widget(name)
             details.add_widget(qty)
@@ -37,7 +32,7 @@ class MainoperatorWindow(BoxLayout):
             details.add_widget(price)
             details.add_widget(total)
 
-            # Update Preview
+            #Update Preview
             pname = "Product One"
             if pcode == '2345':
                 pname = "Product Two"
@@ -54,30 +49,29 @@ class MainoperatorWindow(BoxLayout):
                 prev_text = prev_text[:_prev]
 
             ptarget = -1
-            for i, c in enumerate(self.cart):
+            for i,c in enumerate(self.cart):
                 if c == pcode:
                     ptarget = i
 
             if ptarget >= 0:
                 pqty = self.qty[ptarget]+1
                 self.qty[ptarget] = pqty
-                expr = '%s\t\tx\d\t' % (pname)
+                expr = '%s\t\tx\d\t'%(pname)
                 rexpr = pname+'\t\tx'+str(pqty)+'\t'
-                nu_text = re.sub(expr, rexpr, prev_text)
+                nu_text = re.sub(expr,rexpr,prev_text)
                 preview.text = nu_text + purchase_total
             else:
                 self.cart.append(pcode)
                 self.qty.append(1)
-                nu_preview = '\n'.join(
-                    [prev_text, pname+'\t\tx'+pqty+'\t\t'+str(pprice), purchase_total])
+                nu_preview = '\n'.join([prev_text,pname+'\t\tx'+pqty+'\t\t'+str(pprice),purchase_total])
                 preview.text = nu_preview
 
 
-class MainoperatorApp(App):
+
+class OperatorApp(App):
     def build(self):
-        return MainoperatorWindow()
+        return OperatorWindow()
 
-
-if __name__ == "__main__":
-    oa = MainoperatorApp()
+if __name__=="__main__":
+    oa = OperatorApp()
     oa.run()
